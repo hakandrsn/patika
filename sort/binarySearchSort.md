3fb9ac8442ddcd1c2da226b74b255e68406ee35d
Hakan D <68669489+hakandrsn@users.noreply.github.com>
2023-01-05 20:44:42 +0300

    asd

---
+++ sort/binarySearchSort.md
@@ -0,0 +1,15 @@
+
+# [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary Search Tree aşamaları
+
+1. 7 root olarak atanır.
+2. 5 rootun solunda yer alır.
+3. 1 sol childın solunda yer alır.
+4. 8 rootun sağında yer alır.
+5. 3 sol childın sağında yer alır.
+6. 6 sağ childın solunda yer alır.
+7. 0 sol childın sol childın solunda yer alır.
+8. 9 sağ childın sağında yer alır.
+9. 4 sol childın sağ childın sağında yer alır.
+10. 2 sol childın sağ childın sağ childın solunda yer alır.
+
+big-O gösterimi : O(log(n)) ' dur.
--- sort/insertionSort.js
+++ sort/insertionSort.js
@@ -38,26 +38,3 @@ const insertionMethod = (array) => {
 
 // Time complexity average case durumuna girer dizinin ortalarında yer alıyor.
 
-const selection = [7, 3, 5, 8, 2, 9, 4, 15, 6];
-const selectionSort = (arr) => {
-    let i, j, min;
-    for (i = 0; i < selection.length - 1; i++) {
-        min = i;
-        for (j = i + 1; j < selection.length; j++)
-            if (arr[j] < arr[min])
-                min = j
-
-        let t = arr[min]
-        arr[min] = arr[i]
-        arr[i] = t;
-
-
-    }
-     console.log(arr);
-
-}
-//  2, 3, 4,  5, 6,7, 8, 9, 15
-//2, 3,  5, 8, 7,9, 4, 15, 6
-//  2, 3,  5, 8, 7,9, 4, 15, 6
-// 2, 3,  4, 8, 7,9, 5, 15, 6
-selectionSort(selection)
